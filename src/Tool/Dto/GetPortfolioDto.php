76019cd1ffabbad7c5ff73469527efdaf28b5f08
prikotov <prikotov@gmail.com>
2025-06-13 22:00:29 +0700

    Implement portfolio mapping

--- src/Tool/Dto/GetPortfolioDto.php
+++ src/Tool/Dto/GetPortfolioDto.php
@@ -6,7 +6,21 @@ namespace App\Tool\Dto;
 
 class GetPortfolioDto
 {
-    public function __construct()
-    {
+    public function __construct(
+        public StringValueDto $accountId,
+        public FloatValueDto $totalAmountShares,
+        public FloatValueDto $totalAmountBonds,
+        public FloatValueDto $totalAmountEtf,
+        public FloatValueDto $totalAmountCurrencies,
+        public FloatValueDto $totalAmountFutures,
+        public FloatValueDto $totalAmountOptions,
+        public FloatValueDto $totalAmountSp,
+        public FloatValueDto $totalAmountPortfolio,
+        public FloatValueDto $expectedYield,
+        public IntValueDto $positionsCount,
+        public IntValueDto $virtualPositionsCount,
+        public FloatValueDto $dailyYield,
+        public FloatValueDto $dailyYieldRelative,
+    ) {
     }
 }
